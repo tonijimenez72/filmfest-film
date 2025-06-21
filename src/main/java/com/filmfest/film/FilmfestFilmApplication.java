6ede9dcabeba48c22652e56c1be761e4e7cce464
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2025-06-21 13:54:00 +0200

    Initial commit

---
+++ src/main/java/com/filmfest/film/FilmfestFilmApplication.java
@@ -0,0 +1,13 @@
+package com.filmfest.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmfestFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmfestFilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filmfest/film/FilmfestFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmfest.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmfestFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
