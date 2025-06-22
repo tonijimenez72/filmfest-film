d10926239c24aa57cb3e6c1ba6424678f152b3e5
tonijimenez72 <156689808+tonijimenez72@users.noreply.github.com>
2025-06-23 00:26:11 +0200

    feat: add film url service

--- src/main/java/com/filmfest/film/service/FilmUrlService.java
+++ src/main/java/com/filmfest/film/service/FilmUrlService.java
@@ -1,2 +1,10 @@
-package com.filmfest.film.service;public class FilmUrlService {
+package com.filmfest.film.service;
+
+import com.filmfest.film.dto.FilmUrlDto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface FilmUrlService {
+    Mono<FilmUrlDto> save(FilmUrlDto filmUrlDto);
+    Flux<FilmUrlDto> findAll();
 }
--- src/main/java/com/filmfest/film/service/impl/FilmUrlServiceImpl.java
+++ src/main/java/com/filmfest/film/service/impl/FilmUrlServiceImpl.java
@@ -1,2 +1,31 @@
-package com.filmfest.film.service.impl;public class FilmUrlServiceImpl {
+package com.filmfest.film.service.impl;
+
+import com.filmfest.film.dto.FilmUrlDto;
+import com.filmfest.film.mapper.FilmUrlMapper;
+import com.filmfest.film.model.FilmUrl;
+import com.filmfest.film.repository.FilmUrlRepository;
+import com.filmfest.film.service.FilmUrlService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class FilmUrlServiceImpl implements FilmUrlService {
+
+    private final FilmUrlRepository filmUrlRepository;
+
+    @Override
+    public Mono<FilmUrlDto> save(FilmUrlDto filmUrlDto) {
+        FilmUrl entity = FilmUrlMapper.toEntity(filmUrlDto);
+        return filmUrlRepository.save(entity)
+                .map(FilmUrlMapper::toDto);
+    }
+
+    @Override
+    public Flux<FilmUrlDto> findAll() {
+        return filmUrlRepository.findAll()
+                .map(FilmUrlMapper::toDto);
+    }
 }
